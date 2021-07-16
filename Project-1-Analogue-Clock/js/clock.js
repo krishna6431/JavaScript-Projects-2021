30d552f4887d4221c1148841aea206ea80d82abf
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-07-17 03:51:39 +0530

    updated clock.js

--- Project-1-Analogue-Clock/js/clock.js
+++ Project-1-Analogue-Clock/js/clock.js
@@ -5,7 +5,7 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
 //move canvas to x,y axis 
-ctx.translate(750, 350);
+ctx.translate(radius, radius);
 radius = radius * 0.90;
 drawClock();
 
