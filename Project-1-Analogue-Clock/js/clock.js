05c84305be71d7a96047d1d13a7faf87968853d0
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-07-18 04:05:51 +0530

    added face to the clock

--- Project-1-Analogue-Clock/js/clock.js
+++ Project-1-Analogue-Clock/js/clock.js
@@ -9,12 +9,33 @@ ctx.translate(radius, radius);
 radius = radius * 0.90;
 drawClock();
 
+// function olderdrawClock() {
+//     // arc(x, y, r, startangle, endangle) - creates an arc / curve.To create a circle with arc(): Set start angle to 0 and end angle to 2 * Math.PI.The x and y parameters define the x - and y - coordinates of the center of the circle.The r parameter defines the radius of the circle
+//     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+//     ctx.fillStyle = "White";
+//     ctx.fill();
+// }
+
 function drawClock() {
-    // arc(x, y, r, startangle, endangle) - creates an arc / curve.To create a circle with arc(): Set start angle to 0 and end angle to 2 * Math.PI.The x and y parameters define the x - and y - coordinates of the center of the circle.The r parameter defines the radius of the circle
-    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "White";
-    ctx.fill();
+    drawFace(ctx, radius);
 }
 
-
+function drawFace(ctx, radius) {
+    var grad;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
 
