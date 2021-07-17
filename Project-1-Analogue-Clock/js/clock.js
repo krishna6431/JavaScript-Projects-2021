047505a76f6fa442ea11b970aa2d45d2900c0b21
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-07-18 04:53:17 +0530

    added clock numbers

--- Project-1-Analogue-Clock/js/clock.js
+++ Project-1-Analogue-Clock/js/clock.js
@@ -18,6 +18,7 @@ drawClock();
 
 function drawClock() {
     drawFace(ctx, radius);
+    drawNumber(ctx, radius);
 }
 
 function drawFace(ctx, radius) {
@@ -39,3 +40,27 @@ function drawFace(ctx, radius) {
     ctx.fill();
 }
 
+// adding clock numbers to the clock  
+
+function drawNumber(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for (num = 1; num < 13; num++) {
+
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
+
+}
+
+//Drawing Time and Hands for Clock
+function
