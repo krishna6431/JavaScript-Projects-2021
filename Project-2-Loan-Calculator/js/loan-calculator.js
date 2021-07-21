31239e1ca42a7f7e3644889210521e6b0a032d23
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-07-22 03:46:42 +0530

    added some functionality

--- Project-2-Loan-Calculator/js/loan-calculator.js
+++ Project-2-Loan-Calculator/js/loan-calculator.js
@@ -1,1 +1,14 @@
-// this is javascript file
+// < !--this project is created by krishna6431-- >
+
+function computeLoan() {
+
+    var amount = document.getElementById('amount').value;
+    var interestRate = document.getElementById('interestRate').value;
+    var months = document.getElementById('months').value;
+    var interest = (amount * (interestRate * 0.01)) / months;
+    var payment = ((amount / months) + interest).toFixed(2);
+    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    // \B is used to look into word boudry
+    // ?= tells it to find what the group is looking for 
+    document.getElementById('payment').innerHTML = "Monthly Payment = $" + payment;
+}
