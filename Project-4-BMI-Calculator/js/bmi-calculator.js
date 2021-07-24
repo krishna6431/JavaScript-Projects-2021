ccf920814b9be0838eb1ffc6483a496fd1e126e3
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-07-24 08:29:40 +0530

    added bmi js

---
+++ Project-4-BMI-Calculator/js/bmi-calculator.js
@@ -0,0 +1,27 @@
+// BMI = KG / (H / 100 * H / 100);
+
+document.getElementById("submit").addEventListener("click", bmiCalculator);
+
+function bmiCalculator() {
+
+    var cm = parseInt(document.getElementById("cm").value);
+    var kg = parseInt(document.getElementById("kg").value);
+
+    var bmi;
+    var newCm = parseFloat(cm / 100);
+
+    bmi = kg / (newCm * newCm);
+    bmi = bmi.toFixed(1);
+    console.log(bmi);
+    var check;
+    if (bmi < 18.6) {
+        check = "UnderWeight";
+    }
+    else if (bmi >= 18.6 && bmi <= 24.9) {
+        check = "Normal Range";
+    }
+    else {
+        check = "OverWeight";
+    }
+    document.getElementById("result").innerHTML = bmi.toString() + " " + check;
+}
