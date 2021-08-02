5946088a6b9ec7957b70121ee816861fead3b9ad
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-08-03 04:52:12 +0530

    making the ball move

--- Project-7-Bouncing-Ball/js/script.js
+++ Project-7-Bouncing-Ball/js/script.js
@@ -4,14 +4,20 @@ var canvas = document.getElementById("mycanvas");
 //create 2d context from canvas
 var ctx = canvas.getContext("2d");
 
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 //creating ball for game 
 function draw() {
     ctx.beginPath(); //begin path from drawing ball
     // arc(x, y, radius, startAngle, endAngle, false / true(direction))
-    ctx.arc(220, 140, 20, 0, Math.PI * 2, false);
+    ctx.arc(x, y, 20, 0, Math.PI * 2, false);
     ctx.fillStyle = "red"; //give color to shape circle
     ctx.fill(); // fill color to shape
     ctx.closePath(); // close of path 
+    x += dx;
+    y += dy;
 }
 //infinte loop
 // setInterval(method name , time in ms)
