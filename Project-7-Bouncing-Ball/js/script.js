e71a4336e941ea977228a51f1229a95a2f257061
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-08-03 22:22:23 +0530

    create variable for paddle

--- Project-7-Bouncing-Ball/js/script.js
+++ Project-7-Bouncing-Ball/js/script.js
@@ -8,7 +8,9 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
-
+var paddleHeight = 12;
+var paddleWidth = 72;
+var paddleX = (canvas.width - paddleWidth) / 2;
 function drawBall() {
     ctx.beginPath(); //begin path from drawing ball
     // arc(x, y, radius, startAngle, endAngle, false / true(direction))
